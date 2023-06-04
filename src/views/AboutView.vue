a41fc42e5dce66e6f55b910c4ce19b9e8b69567f
Janik F <53714687+Shiroikage@users.noreply.github.com>
2023-06-04 22:01:54 +0200

    no f. idea; two

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is a Project by Maurice and Janik</h1>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+let inputText = ref('4au6goX6kuGGbMGUqbbRO6')
+let inputUrl = ref("https://open.spotify.com/embed/playlist/" + inputText.value + "?utm_source=generator")
+</script>
+
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <input placeholder="enter the Playlist-ID" class="w-full py-2 border border-indigo-500 rounded"/>
-    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4au6goX6kuGGbMGUqbbRO6?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <input v-model="inputText" type="input" placeholder="enter the Playlist-ID" class="w-full py-2 border border-indigo-500 rounded"/>
+    <button @click="something">Hey</button> <!--no fuckign idea how to update the iframe and therefore inputUrl-->
+    <iframe style="border-radius:12px" :src="inputUrl" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <div>{{inputUrl}}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
