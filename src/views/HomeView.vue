39b4d8b7e47d1f99f95e647be1b4c3831e7cac19
Shiroikage <cyberfink1@gmail.com>
2023-06-06 14:44:54 +0200

    fixed that iFrame, still updates to much tho, need to add button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 let inputText = ref('4au6goX6kuGGbMGUqbbRO6')
-let inputUrl = ref("https://open.spotify.com/embed/playlist/" + inputText.value + "?utm_source=generator")
+const inputUrl = computed(() => {
+  return `https://open.spotify.com/embed/playlist/${inputText.value}?utm_source=generator`;
+})
 </script>
 
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <input v-model="inputText" type="input" placeholder="enter the Playlist-ID" class="w-full py-2 border border-indigo-500 rounded"/>
-    <button @click="something">Hey</button> <!--no fuckign idea how to update the iframe and therefore inputUrl-->
+    <button @click="something">Hey</button>
     <iframe style="border-radius:12px" :src="inputUrl" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <div>{{inputText}}</div>
     <div>{{inputUrl}}</div>
   </div>
 </template>
