80363f062a6ceafdf501954f21f4d395e06a3958
Janik F <53714687+Shiroikage@users.noreply.github.com>
2023-07-06 17:25:31 +0200

    playlist isnt init when called in template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="home">
     <Locator location="Home"/>
-    <input>
+    <!--<input v-model="inputText" type="input" placeholder="enter the Playlist-ID" class="w-full py-2 border border-indigo-500 rounded"/>-->
     <FancyCounter :number="playlist.length"/>
     <div class="playlistRender" v-for="song in playlist" :key="song.id">
-      <SongPreview :linkId="song.track_id"/>
+      <SongPreview :linkId="song.value"/>
     </div>
   </div>
 </template>
 
 <script setup>
-import Locator from '@/components/Locator.vue'
+import Locator from '@/components/Locator.vue';
 import SongPreview from "@/components/SongPreview.vue";
 import FancyCounter from "@/components/FancyCounter.vue";
-import {ref} from "vue";
+import { onBeforeMount } from 'vue';
+import { ref } from 'vue';
 
-const playlist = ref([
-  {track_id: '6pDFS313jjuoDKq4DeUvOH'}, //TODO: obv dont hardcode tracks lol
-  {track_id: '3IVrlcM5gDoj3lghHY9m7I'}
-])
+const playlist = getSongs('4au6goX6kuGGbMGUqbbRO6');
+
+function getSongs(playlistId){
+      const baseUrl = 'http://localhost:8080'
+      const endpoint = baseUrl + '/playlist/' + playlistId
+      const requestOptions = {
+        method: 'GET',
+        mode: 'cors',
+      }
+      fetch(endpoint, requestOptions)
+      .then(response => {return response.json();})
+      .then(response => console.log('songs in playlist: ' + response.length))
+      .catch(err => console.error(err))
+}
 </script>
 
 <style>
