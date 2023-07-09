177630e1efaf62b8ef3c2bcf5666adeb9b688fa8
Janik F <53714687+Shiroikage@users.noreply.github.com>
2023-07-09 21:52:02 +0200

    typo fml

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
-import MyComponent from '@/components/MyComponent.vue';
+import MyComponent from '@/components/disHeading.vue';
 
 
 describe('MyComponent', () => {
