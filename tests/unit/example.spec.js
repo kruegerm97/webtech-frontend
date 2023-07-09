e434982e11ae2ce8a593ca2105f8eded0a1b5ab5
kruegerm97 <kruegermaurice97@gmail.com>
2023-07-09 21:30:58 +0200

    some test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,8 +3,8 @@ import Locator from '@/components/Locator.vue'
 
 describe('Locator.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const msg = 'Home'
+    const wrapper = shallowMount(Locator, {
       props: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
